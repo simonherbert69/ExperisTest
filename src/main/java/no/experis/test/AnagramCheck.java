71c0444cb3d5929ce5f3aa2b8514ac4a9aa9f30b
Simon Herbert <simonherbert69@gmail.com>
2021-02-12 10:51:39 +0100

    Jira-2 created correct java code structure.

---
+++ src/main/java/no/experis/test/AnagramCheck.java
@@ -0,0 +1,24 @@
+package no.experis.test;
+
+import java.util.regex.Pattern;
+
+public class AnagramCheck {
+
+    public boolean isAnagram(String strA, String strB) {
+        // Cleaning the strings (remove white spaces and convert to lowercase)
+        strA = strA.replaceAll("\\s+","").toLowerCase();
+        strB = strB.replaceAll("\\s+","").toLowerCase();
+
+        // Check every char of strA and removes first occurence of it in strB
+        for (int i = 0; i < strA.length(); i++ ) {
+            if (strB.equals("")) return false;  // strB is already empty : not an anagram
+            strB = strB.replaceFirst(Pattern.quote("" + strA.charAt(i)), "");
+        }
+
+        // if strB is empty we have an anagram
+        return strB.equals("");
+    }
+
+
+}
+//            System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
