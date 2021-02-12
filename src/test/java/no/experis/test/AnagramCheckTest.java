9f4c1e1b4773c7006e1e55d55c569d8010622faa
Simon Herbert <simonherbert69@gmail.com>
2021-02-12 11:03:46 +0100

    looking more like a test ;-)

--- src/test/java/no/experis/test/AnagramCheckTest.java
+++ src/test/java/no/experis/test/AnagramCheckTest.java
@@ -4,20 +4,9 @@ import java.util.regex.Pattern;
 
 public class AnagramCheckTest {
 
-    public boolean isAnagram(String strA, String strB) {
-        // Cleaning the strings (remove white spaces and convert to lowercase)
-        strA = strA.replaceAll("\\s+","").toLowerCase();
-        strB = strB.replaceAll("\\s+","").toLowerCase();
+   public void testAnagram() {
 
-        // Check every char of strA and removes first occurence of it in strB
-        for (int i = 0; i < strA.length(); i++ ) {
-            if (strB.equals("")) return false;  // strB is already empty : not an anagram
-            strB = strB.replaceFirst(Pattern.quote("" + strA.charAt(i)), "");
-        }
-
-        // if strB is empty we have an anagram
-        return strB.equals("");
-    }
+   }
 
 
 }
