535681745c6830c95eef747538f41b18c5d6240f
Simon Herbert <simonherbert69@gmail.com>
2021-02-12 11:17:43 +0100

    Jira-4 add test suppport - should use gradle or Maven ;-)

--- src/test/java/no/experis/test/AnagramCheckTest.java
+++ src/test/java/no/experis/test/AnagramCheckTest.java
@@ -1,5 +1,9 @@
 package no.experis.test;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.util.regex.Pattern;
 
 public class AnagramCheckTest {
@@ -9,5 +13,16 @@ public class AnagramCheckTest {
    }
 
 
+   @BeforeEach
+   void setUp() {
+   }
+
+   @AfterEach
+   void tearDown() {
+   }
+
+   @Test
+   void isAnagram() {
+   }
 }
 //            System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
